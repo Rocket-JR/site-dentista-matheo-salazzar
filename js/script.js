a875ff9dd438330b369ae934ffba0cfed27d82ba
Bruno Guilherme <brunogui29@outlook.com>
2025-03-27 20:22:31 -0300

    feat:criação da 5 seção

--- js/script.js
+++ js/script.js
@@ -20,3 +20,31 @@ document.addEventListener('DOMContentLoaded', function() {
         observer.observe(section);
     });
 });
+
+const slider = document.querySelector('.slider');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+});
+
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 2; // Ajuste a sensibilidade
+    slider.scrollLeft = scrollLeft - walk;
+});
